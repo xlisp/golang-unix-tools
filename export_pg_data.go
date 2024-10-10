0977c93180a85a22190d434898b160271f0c142a
stevechan <chanshunli@gmail.com>
2024-10-10 10:55:06 +0800

    golong write export markdown file from pg blogs table:  db: text2code  user: postgrest, pwd: 123456, column: id is file name , name is markdown title , content is markdown content.

---
+++ export_pg_data.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Database connection parameters
+	connStr := "user=postgrest password=123456 dbname=text2code sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Query to get the blog data
+	rows, err := db.Query("SELECT id, name, content FROM blogs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	// Iterate through the rows
+	for rows.Next() {
+		var id int
+		var name, content string
+
+		// Scan the row data into variables
+		if err := rows.Scan(&id, &name, &content); err != nil {
+			log.Fatal(err)
+		}
+
+		// Create markdown file with the id as the filename
+		fileName := fmt.Sprintf("%d.md", id)
+		file, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		// Write markdown content to the file
+		markdownContent := fmt.Sprintf("# %s\n\n%s", name, content)
+		_, err = file.WriteString(markdownContent)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Markdown file created: %s\n", fileName)
+	}
+
+	// Check for errors after iterating
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Markdown files exported successfully.")
+}
+
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
